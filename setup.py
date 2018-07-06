00fd696dc6992c6091dc54ad0f18d7e3de40b949
Richard Chien <richardchienthebest@gmail.com>
2018-07-06 19:24:34 +0800

    Update dependency

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
     description='A QQ bot framework',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['aiocqhttp>=0.5', 'aiocache',
-                      'apscheduler', 'sqlalchemy'],
+    install_requires=['aiocqhttp>=0.5', 'aiocache'],
     python_requires='>=3.6',
     platforms='any',
     classifiers=(
