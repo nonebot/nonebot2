2caf90646bc45b6e603c529d1d4b69eb035472ca
Richard Chien <richardchienthebest@gmail.com>
2018-07-21 21:59:51 +0800

    Adjust log format

--- none/log.py
+++ none/log.py
@@ -11,6 +11,6 @@ import sys
 logger = logging.getLogger('none')
 default_handler = logging.StreamHandler(sys.stdout)
 default_handler.setFormatter(logging.Formatter(
-    '[%(asctime)s] %(levelname)s: %(message)s'
+    '[%(asctime)s %(name)s] %(levelname)s: %(message)s'
 ))
 logger.addHandler(default_handler)
