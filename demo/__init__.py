5ea5a616a5f360b72f8c45796c28b908ae15f2b2
Richard Chien <richardchienthebest@gmail.com>
2018-08-15 23:43:33 +0800

    Restore demo

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,20 +1,13 @@
-# from os import path
-#
-# import none
-# from demo import config
-#
-# none.init(config)
-# app = none.get_bot().asgi
-#
-# if __name__ == '__main__':
-#     none.load_builtin_plugins()
-#     none.load_plugins(path.join(path.dirname(__file__), 'plugins'),
-#                       'demo.plugins')
-#     none.run(host=config.HOST, port=config.PORT)
+from os import path
 
 import none
+from demo import config
+
+none.init(config)
+app = none.get_bot().asgi
 
 if __name__ == '__main__':
-    none.init()
     none.load_builtin_plugins()
-    none.run(host='0.0.0.0', port=8080)
+    none.load_plugins(path.join(path.dirname(__file__), 'plugins'),
+                      'demo.plugins')
+    none.run(host=config.HOST, port=config.PORT)
