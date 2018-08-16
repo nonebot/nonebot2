d5e506d6e38abf7c81e83c66a3c23da06c94aab8
Richard Chien <richardchienthebest@gmail.com>
2018-08-17 01:04:04 +0800

    Little fix

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
     ),
 )
