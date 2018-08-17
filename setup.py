ec868b6a8082792926ce366fc29726837cf55776
Richard Chien <richardchienthebest@gmail.com>
2018-08-18 01:22:46 +0800

    Bump version to v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='none-bot',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(include=('none', 'none.*')),
     url='https://github.com/richardchien/none-bot',
     license='MIT License',
