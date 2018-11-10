e0cebd6a7a59093d83dda20c54397c5f2446fde2
Richard Chien <richardchienthebest@gmail.com>
2018-11-10 10:16:59 +0800

    Fix typing

--- none/typing.py
+++ none/typing.py
@@ -3,5 +3,5 @@ from typing import Union, List, Dict, Any, Sequence, Callable, Tuple
 Context_T = Dict[str, Any]
 Message_T = Union[str, Dict[str, Any], List[Dict[str, Any]]]
 Expression_T = Union[str, Sequence[str], Callable]
-CommandName_T = Tuple[str]
+CommandName_T = Tuple[str, ...]
 CommandArgs_T = Dict[str, Any]
