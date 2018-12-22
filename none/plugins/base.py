be2e44bc967f6408b24d6ca9c469c82db81dcba3
Richard Chien <richardchienthebest@gmail.com>
2018-12-22 21:22:58 +0800

    Remove "switch" command

--- none/plugins/base.py
+++ none/plugins/base.py
@@ -1,8 +1,5 @@
-import asyncio
-
 from none import on_command, CommandSession, permission as perm
-from none.command import kill_current_session
-from none.message import unescape, Message, handle_message
+from none.message import unescape
 
 
 @on_command('echo')
@@ -11,18 +8,6 @@ async def echo(session: CommandSession):
 
 
 @on_command('say', permission=perm.SUPERUSER)
-async def _(session: CommandSession):
+async def say(session: CommandSession):
     await session.send(
         unescape(session.get_optional('message') or session.current_arg))
-
-
-@on_command('switch', privileged=True)
-async def _(session: CommandSession):
-    kill_current_session(session.bot, session.ctx)
-
-    msg = Message(session.current_arg)
-    await session.send(msg)
-
-    ctx = session.ctx.copy()
-    ctx['message'] = msg
-    asyncio.ensure_future(handle_message(session.bot, ctx))
