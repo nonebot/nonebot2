932d178c15d45c344406785ac48b8d51f7cb7abd
Richard Chien <richardchienthebest@gmail.com>
2020-02-10 12:17:00 +0800

    Fix compatibility with Quart 0.11

--- nonebot/__init__.py
+++ nonebot/__init__.py
@@ -78,7 +78,7 @@ def init(config_object: Optional[Any] = None) -> None:
     _bot.server_app.before_serving(_start_scheduler)
 
 
-def _start_scheduler():
+async def _start_scheduler():
     if scheduler and not scheduler.running:
         scheduler.configure(_bot.config.APSCHEDULER_CONFIG)
         scheduler.start()
