e0cba7c20c595643225c356441f31e5db472840a
yanyongyu <yanyongyu_1@126.com>
2020-09-27 18:20:39 +0800

    :art: add command group to entry file

--- nonebot/__init__.py
+++ nonebot/__init__.py
@@ -213,5 +213,5 @@ async def _start_scheduler():
 
 
 from nonebot.plugin import on_message, on_notice, on_request, on_metaevent
-from nonebot.plugin import on_startswith, on_endswith, on_command, on_regex
+from nonebot.plugin import on_startswith, on_endswith, on_command, on_regex, CommandGroup
 from nonebot.plugin import load_plugin, load_plugins, load_builtin_plugins, get_loaded_plugins
