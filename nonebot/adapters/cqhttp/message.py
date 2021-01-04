877fa1a75adaf7412b8f33505ea83ad0995dcde4
Artin <lengthmin@gmail.com>
2021-01-04 22:24:43 +0800

    :bug: Fix cqhttp `Message` null parameter

--- nonebot/adapters/cqhttp/message.py
+++ nonebot/adapters/cqhttp/message.py
@@ -222,29 +222,29 @@ class Message(BaseMessage):
             for seg in msg:
                 yield MessageSegment(seg["type"], seg.get("data") or {})
             return
+        elif isinstance(msg, str):
+            def _iter_message(msg: str) -> Iterable[Tuple[str, str]]:
+                text_begin = 0
+                for cqcode in re.finditer(
+                        r"\[CQ:(?P<type>[a-zA-Z0-9-_.]+)"
+                        r"(?P<params>"
+                        r"(?:,[a-zA-Z0-9-_.]+=[^,\]]+)*"
+                        r"),?\]", msg):
+                    yield "text", msg[text_begin:cqcode.pos + cqcode.start()]
+                    text_begin = cqcode.pos + cqcode.end()
+                    yield cqcode.group("type"), cqcode.group("params").lstrip(",")
+                yield "text", msg[text_begin:]
 
-        def _iter_message(msg: str) -> Iterable[Tuple[str, str]]:
-            text_begin = 0
-            for cqcode in re.finditer(
-                    r"\[CQ:(?P<type>[a-zA-Z0-9-_.]+)"
-                    r"(?P<params>"
-                    r"(?:,[a-zA-Z0-9-_.]+=[^,\]]+)*"
-                    r"),?\]", msg):
-                yield "text", msg[text_begin:cqcode.pos + cqcode.start()]
-                text_begin = cqcode.pos + cqcode.end()
-                yield cqcode.group("type"), cqcode.group("params").lstrip(",")
-            yield "text", msg[text_begin:]
-
-        for type_, data in _iter_message(msg):
-            if type_ == "text":
-                if data:
-                    # only yield non-empty text segment
-                    yield MessageSegment(type_, {"text": unescape(data)})
-            else:
-                data = {
-                    k: unescape(v) for k, v in map(
-                        lambda x: x.split("=", maxsplit=1),
-                        filter(lambda x: x, (
-                            x.lstrip() for x in data.split(","))))
-                }
-                yield MessageSegment(type_, data)
+            for type_, data in _iter_message(msg):
+                if type_ == "text":
+                    if data:
+                        # only yield non-empty text segment
+                        yield MessageSegment(type_, {"text": unescape(data)})
+                else:
+                    data = {
+                        k: unescape(v) for k, v in map(
+                            lambda x: x.split("=", maxsplit=1),
+                            filter(lambda x: x, (
+                                x.lstrip() for x in data.split(","))))
+                    }
+                    yield MessageSegment(type_, data)
