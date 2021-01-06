b6bf4775516ecc4e58d6e0595ee7bd8691bb502f
yanyongyu <yanyongyu_1@126.com>
2021-01-06 18:29:24 +0800

    :alembic: add got receive overload #142

--- nonebot/matcher.py
+++ nonebot/matcher.py
@@ -294,17 +294,26 @@ class Matcher(metaclass=MatcherMeta):
           * 无
         """
 
-        async def _receive(bot: "Bot", event: "Event",
-                           state: T_State) -> NoReturn:
+        async def _receive(bot: "Bot", event: "Event") -> NoReturn:
             raise PausedException
+
+        cls.process_handler(_receive)
 
         if cls.handlers:
             # 已有前置handlers则接受一条新的消息，否则视为接收初始消息
             cls.append_handler(_receive)
 
         def _decorator(func: T_Handler) -> T_Handler:
+            cls.process_handler(func)
             if not cls.handlers or cls.handlers[-1] is not func:
                 cls.append_handler(func)
+
+            _receive.__params__.update({
+                "bot":
+                    func.__params__["bot"],
+                "event":
+                    func.__params__["event"] or _receive.__params__["event"]
+            })
 
             return func
 
@@ -367,6 +376,25 @@ class Matcher(metaclass=MatcherMeta):
                         del state["_current_key"]
 
                 cls.append_handler(wrapper)
+
+                wrapper.__params__.update({
+                    "bot":
+                        func.__params__["bot"],
+                    "event":
+                        func.__params__["event"] or wrapper.__params__["event"]
+                })
+                _key_getter.__params__.update({
+                    "bot":
+                        func.__params__["bot"],
+                    "event":
+                        func.__params__["event"] or wrapper.__params__["event"]
+                })
+                _key_parser.__params__.update({
+                    "bot":
+                        func.__params__["bot"],
+                    "event":
+                        func.__params__["event"] or wrapper.__params__["event"]
+                })
 
             return func
 
