2e75671d5617297a951b19748893d78afefa9e27
yanyongyu <yanyongyu_1@126.com>
2021-01-26 15:03:37 +0800

    :alembic: add processor to ensure single matcher

--- nonebot/plugins/single_session.py
+++ nonebot/plugins/single_session.py
@@ -5,16 +5,22 @@ from nonebot.matcher import Matcher
 from nonebot.adapters import Bot, Event
 from nonebot.message import run_preprocessor, run_postprocessor, IgnoredException
 
-_running_matcher: Dict[str, bool] = {}
+_running_matcher: Dict[str, int] = {}
 
 
 @run_preprocessor
 async def _(matcher: Matcher, bot: Bot, event: Event, state: T_State):
-    # TODO
-    pass
+    session_id = event.get_session_id()
+    event_id = id(event)
+
+    if _running_matcher.get(session_id, None) != event_id:
+        raise IgnoredException("Annother matcher running")
+
+    _running_matcher[session_id] = event_id
 
 
 @run_postprocessor
 async def _(matcher: Matcher, exception: Optional[Exception], bot: Bot, event: Event, state: T_State):
-    # TODO
-    pass
+    session_id = event.get_session_id()
+    if session_id in _running_matcher:
+        del _running_matcher[session_id]
