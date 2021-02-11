31b8a5ff771ac33550905b7482e25673b2055433
yanyongyu <yanyongyu_1@126.com>
2021-02-11 17:29:16 +0800

    :building_construction: change nonebot project structure

--- nonebot/adapters/__init__.py
+++ nonebot/adapters/__init__.py
@@ -20,6 +20,17 @@ if TYPE_CHECKING:
     from nonebot.config import Config
     from nonebot.drivers import Driver, WebSocket
 
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+    del pkg_resources
+except ImportError:
+    import pkgutil
+    __path__: Iterable[str] = pkgutil.extend_path(__path__, __name__)
+    del pkgutil
+except Exception:
+    pass
+
 
 class Bot(abc.ABC):
     """
