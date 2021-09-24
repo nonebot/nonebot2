b31d04dc5dfe526899515ebbe3038625defbd02f
StarHeartHunt <starheart233@gmail.com>
2021-09-24 20:26:50 +0800

    :art: improve code style

--- tests/test_plugins/test_feishu.py
+++ tests/test_plugins/test_feishu.py
@@ -1,12 +1,11 @@
-from nonebot.adapters.feishu.event import MessageEvent
-from nonebot.rule import to_me
 from nonebot.plugin import on_command
-from nonebot.adapters.feishu import Bot as FeishuBot, MessageSegment, MessageEvent
+from nonebot.typing import T_State
+from nonebot.adapters.feishu import Bot as FeishuBot, MessageEvent
 
 helper = on_command("say")
 
 
 @helper.handle()
-async def feishu_helper(bot: FeishuBot, event: MessageEvent):
+async def feishu_helper(bot: FeishuBot, event: MessageEvent, state: T_State):
     message = event.get_message()
     await helper.finish(message, at_sender=True)
