d773fea709066d7b968298647e9b3d19a7f7efa3
yanyongyu <yanyongyu_1@126.com>
2021-12-25 18:46:44 +0800

    :bug: fix echo message error

--- nonebot/plugins/echo.py
+++ nonebot/plugins/echo.py
@@ -1,11 +1,11 @@
 from nonebot.rule import to_me
 from nonebot.adapters import Message
+from nonebot.params import CommandArg
 from nonebot.plugin import on_command
-from nonebot.params import EventMessage
 
 echo = on_command("echo", to_me())
 
 
 @echo.handle()
-async def echo_escape(message: Message = EventMessage()):
+async def echo_escape(message: Message = CommandArg()):
     await echo.send(message=message)
