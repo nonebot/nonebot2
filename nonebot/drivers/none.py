34a454eb9199d0c93baae1443c28f06b4978bff1
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-03-28 16:56:34 +0000

    :white_check_mark: add coverage condition annotation

--- nonebot/drivers/none.py
+++ nonebot/drivers/none.py
@@ -27,7 +27,7 @@ HANDLED_SIGNALS = (
     signal.SIGINT,  # Unix signal 2. Sent by Ctrl+C.
     signal.SIGTERM,  # Unix signal 15. Sent by `kill <pid>`.
 )
-if WINDOWS:
+if WINDOWS:  # pragma: py-win32
     HANDLED_SIGNALS += (signal.SIGBREAK,)  # Windows signal 21. Sent by Ctrl+Break.
 
 
