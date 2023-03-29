3555e1e2dc4966d4538a0e8af74675cc39fe708c
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-03-29 07:31:25 +0000

    :bug: fix lifespan sequence

--- nonebot/drivers/_lifespan.py
+++ nonebot/drivers/_lifespan.py
@@ -1,4 +1,4 @@
-from typing import Any, Set, Union, Callable, Awaitable, cast
+from typing import Any, List, Union, Callable, Awaitable, cast
 
 from nonebot.utils import run_sync, is_coroutine_callable
 
@@ -9,20 +9,20 @@ LIFESPAN_FUNC = Union[SYNC_LIFESPAN_FUNC, ASYNC_LIFESPAN_FUNC]
 
 class Lifespan:
     def __init__(self) -> None:
-        self._startup_funcs: Set[LIFESPAN_FUNC] = set()
-        self._shutdown_funcs: Set[LIFESPAN_FUNC] = set()
+        self._startup_funcs: List[LIFESPAN_FUNC] = []
+        self._shutdown_funcs: List[LIFESPAN_FUNC] = []
 
     def on_startup(self, func: LIFESPAN_FUNC) -> LIFESPAN_FUNC:
-        self._startup_funcs.add(func)
+        self._startup_funcs.append(func)
         return func
 
     def on_shutdown(self, func: LIFESPAN_FUNC) -> LIFESPAN_FUNC:
-        self._shutdown_funcs.add(func)
+        self._shutdown_funcs.append(func)
         return func
 
     @staticmethod
     async def _run_lifespan_func(
-        funcs: Set[LIFESPAN_FUNC],
+        funcs: List[LIFESPAN_FUNC],
     ) -> None:
         for func in funcs:
             if is_coroutine_callable(func):
