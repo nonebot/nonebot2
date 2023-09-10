52545079a66603af6d7398c679a6586f33afa056
Akirami <66513481+A-kirami@users.noreply.github.com>
2023-09-10 12:28:32 +0800

    🔊 检查未通过时的日志调整为 TRACE 级别

--- nonebot/message.py
+++ nonebot/message.py
@@ -359,7 +359,7 @@ async def _check_matcher(
 
     try:
         if not await Matcher.check_perm(bot, event, stack, dependency_cache):
-            logger.debug(f"Permission check failed for {Matcher}")
+            logger.trace(f"Permission check failed for {Matcher}")
             return False
     except Exception as e:
         logger.opt(colors=True, exception=e).error(
@@ -369,7 +369,7 @@ async def _check_matcher(
 
     try:
         if not await Matcher.check_rule(bot, event, state, stack, dependency_cache):
-            logger.debug(f"Rule check failed for {Matcher}")
+            logger.trace(f"Rule check failed for {Matcher}")
             return False
     except Exception as e:
         logger.opt(colors=True, exception=e).error(
