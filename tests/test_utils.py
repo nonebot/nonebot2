8a590d24537241fd1447ed04f636c3ab2842a15c
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-10-27 22:45:03 +0800

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, List, Union, Literal, TypeVar
+from typing import Dict, List, Union, Literal, TypeVar, ClassVar
 
 from utils import FakeMessage, FakeMessageSegment
 from nonebot.utils import (
@@ -28,6 +28,7 @@ def test_generic_check_issubclass():
     assert not generic_check_issubclass(Literal[1, 2, "3"], int)
     assert generic_check_issubclass(List[int], list)
     assert generic_check_issubclass(Dict[str, int], dict)
+    assert not generic_check_issubclass(ClassVar[int], int)
     assert generic_check_issubclass(TypeVar("T", int, float), (int, float))
     assert generic_check_issubclass(TypeVar("T", bound=int), (int, float))
 
