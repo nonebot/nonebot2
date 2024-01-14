c8294fccf8dbdbeadc6ddf7099de740abfe7333c
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-01-14 04:33:18 +0000

    :bug: echo send when message empty

---
+++ tests/test_echo.py
@@ -0,0 +1,33 @@
+import pytest
+from nonebug import App
+
+from utils import FakeMessage, FakeMessageSegment, make_fake_event
+
+
+@pytest.mark.asyncio
+async def test_echo(app: App):
+    from nonebot.plugins.echo import echo
+
+    async with app.test_matcher(echo) as ctx:
+        bot = ctx.create_bot()
+
+        message = FakeMessage("/echo 123")
+        event = make_fake_event(_message=message)()
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, FakeMessage("123"), True, bot=bot)
+
+        message = FakeMessageSegment.text("/echo 123") + FakeMessageSegment.image(
+            "test"
+        )
+        event = make_fake_event(_message=message)()
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(
+            event,
+            FakeMessageSegment.text("123") + FakeMessageSegment.image("test"),
+            True,
+            bot=bot,
+        )
+
+        message = FakeMessage("/echo")
+        event = make_fake_event(_message=message)()
+        ctx.receive_event(bot, event)
