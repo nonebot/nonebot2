cf00e08a894b1ab052f454f43a5df97843c531d2
MingxuanGame <MingxuanGame@outlook.com>
2024-02-22 11:13:16 +0800

    :memo: update session_expire_timeout docs

--- nonebot/config.py
+++ nonebot/config.py
@@ -461,7 +461,6 @@ class Config(BaseSettings):
 
     用法:
         ```conf
-        SESSION_EXPIRE_TIMEOUT=120  # 单位: 秒
         SESSION_EXPIRE_TIMEOUT=[DD ][HH:MM]SS[.ffffff]
         SESSION_EXPIRE_TIMEOUT=P[DD]DT[HH]H[MM]M[SS]S  # ISO 8601
         ```
