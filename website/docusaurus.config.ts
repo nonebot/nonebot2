51d11fc45d66bca03a0abe3fdec3e3d29a608e90
StarHeartHunt <starheart233@gmail.com>
2024-10-13 15:25:33 +0800

    :sparkles: changelog sidebar

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -68,7 +68,7 @@ const navbar: Preset.ThemeConfig["navbar"] = {
         { label: "社区", type: "doc", docId: "community/contact" },
         { label: "开源之夏", type: "doc", docId: "ospp/2024" },
         { label: "商店", to: "/store/plugins" },
-        { label: "更新日志", to: "/changelog" },
+        { label: "更新日志", to: "/docs/next/changelog" },
         { label: "论坛", href: "https://discussions.nonebot.dev" },
       ],
     },
