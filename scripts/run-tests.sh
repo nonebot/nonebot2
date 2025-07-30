cb9c2b208d32d26cbe7f8268252f778956cd50a9
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2025-07-30 14:24:36 +0000

    :bug: fix lock error

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # cd to the root of the tests
 cd "$(dirname "$0")/../tests"
--- scripts/setup-envs.sh
+++ scripts/setup-envs.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 echo "Setting up dev environment"
 uv sync --all-extras && uv run pre-commit install && yarn install
